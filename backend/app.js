5d2728f19028a0f1c1cbce9d0c242862fbdbba58
tahha00 <tahha2k9@gmail.com>
2023-11-06 12:44:07 +0000

    add get requests

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require("cors");
 const logger = require("./logger");
 
+//const countries = require(./countries)
 const app = express();
 
 app.use(cors());
@@ -12,6 +13,29 @@ app.use(logger);
 app.get('/', (req, res) => {
     res.send("Hello geothusiasm")
 })
+
+app.get('/countries', (req, res) => {
+    res.send(countries)
+})
+
+app.get('/countries/random', (req, res) => {
+    const randId = Math.floor(Math.random() * countries.length);
+    res.send(countries[randId]);
+})
+
+// let highScores = [];
+
+/* app.post('/highscores', (req, res) => {
+    const { name, score } = req.body;
+
+    if (name && score) {
+        highScores.push({ name, score });
+
+        res.status(201).json({message: 'High score submitted!'});
+    } else {
+        res.status(400).json({ message: 'Invalid name or score' });
+    }
+}); */
 
 
 module.exports = app;
