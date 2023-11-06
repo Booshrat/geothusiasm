ef32792891af20fdbe981e06d3f4c88ce73fa32d
booshrat <booshrat@gmail.com>
2023-11-06 17:06:02 +0000

    add countDown feature

--- frontend/index.js
+++ frontend/index.js
@@ -11,10 +11,28 @@ function buttonsToForm(e){
 
 nameEnter.addEventListener("submit", addToBoard);
 
-let startMessage = document.querySelector("#start-message")
+let startMessage = document.querySelector("#start-message");
 
 function addToBoard(e){
     e.preventDefault()
     nameEnter.remove()
-    startMessage.textContent = "Game Starting in 3"
+    let count = 3;
+    startMessage.textContent = "Game starting in "  + count + " seconds...";
+
+    let countDown = setInterval(function() {
+        count --;
+
+        if (count < 0) {
+            clearInterval(countDown);
+            startGame();
+        } else {
+            startMessage.textContent = "Game starting in " + count + " seconds...";
+        }
+    }, 1000);
+
+    document.querySelector("h1").remove();
+}
+
+function startGame () {
+    startMessage.remove();
 }
