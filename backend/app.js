7466d8f4b1e7be9b6b4ef9eff36dd4cffa8abc8a
booshrat <booshrat@gmail.com>
2023-11-06 12:46:44 +0000

    add get requests for '/' root and 'countries/:id'

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require("cors");
 const logger = require("./logger");
+const countries = require("./flags");
 
 const app = express();
 
@@ -10,8 +11,17 @@ app.use(express.json())
 app.use(logger);
 
 app.get('/', (req, res) => {
-    res.send("Hello geothusiasm")
+    res.send("Welcome to geothusiasm")
 })
 
+app.get('/countries/:id', (req, res) => {
+    const idx = req.params.id;
+
+    if (idx <= 0 || idx > countries.length || isNaN(idx)) {
+        res.status(404).send("Country not found!");
+    } else {
+        res.status(201).send(countries[idx - 1]);
+    }
+})
 
 module.exports = app;
