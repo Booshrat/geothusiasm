d49dfbb750d1fd092a4de50187715a898b750512
booshrat <booshrat@gmail.com>
2023-11-07 14:59:07 +0000

    change styling

--- frontend/index.js
+++ frontend/index.js
@@ -40,7 +40,8 @@ function addToBoard(e){
 function startGame () {
     startMessage.remove();
     randomFlag();
-    document.querySelector("body").style.backgroundImage = "url(./images/earth.jpg)";    
+    document.querySelector(".hero").style.backgroundColor = "rgba(0, 0, 0, 0.85)";   
+    document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, 0.85)";  
 } 
 
 async function randomFlag() {
@@ -100,5 +101,30 @@ function shuffle(array) {
     return array;
 }
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelector('.help');
 
+const openModal = function () {
+ console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnsOpenModal.addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
 
