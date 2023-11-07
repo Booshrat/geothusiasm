a95fe474fe94374856e3c9f4f5b3846b004a313a
Billy <s.pbilly@yahoo.co.uk>
2023-11-07 15:22:28 +0000

    buttons appear with flag

--- frontend/index.js
+++ frontend/index.js
@@ -1,178 +1,174 @@
 const start = document.querySelector("#start");
 const leaderBoard = document.querySelector("#leader-board");
 start.addEventListener("click", buttonsToForm);
-const nameEnter = document.querySelector("#player-name")
+const nameEnter = document.querySelector("#player-name");
 
-function buttonsToForm(e){
-    start.remove()
-    leaderBoard.remove()
-    nameEnter.style.display = "block"
+function buttonsToForm(e) {
+  start.remove();
+  leaderBoard.remove();
+  nameEnter.style.display = "block";
 }
 
 nameEnter.addEventListener("submit", addToBoard);
 
 let startMessage = document.querySelector("#start-message");
 
-function addToBoard(e){
-    e.preventDefault()
-    nameEnter.remove()
-    let count = 3;
-    startMessage.textContent = "Game starting in "  + count + " seconds...";
+function addToBoard(e) {
+  e.preventDefault();
+  nameEnter.remove();
+  let count = 3;
+  startMessage.textContent = "Game starting in " + count + " seconds...";
 
-    let countDown = setInterval(function() {
-        count --;
+  let countDown = setInterval(function () {
+    count--;
 
-        if (count < 0) {
-            clearInterval(countDown);
-            startGame();
-        } else {
-            if (count === 1 ) {
-                startMessage.textContent = "Game starting in " + count + " second...";
-            } else {
-                startMessage.textContent = "Game starting in " + count + " seconds...";
-            }
-        }
-    }, 1000);
+    if (count < 0) {
+      clearInterval(countDown);
+      startGame();
+    } else {
+      if (count === 1) {
+        startMessage.textContent = "Game starting in " + count + " second...";
+      } else {
+        startMessage.textContent = "Game starting in " + count + " seconds...";
+      }
+    }
+  }, 1000);
 
-    document.querySelector("#geo").remove();
+  document.querySelector("#geo").remove();
 }
 
-function startGame () {
-    startMessage.remove();
-    randomFlag();
-    document.querySelector(".hero").style.backgroundColor = "rgba(0, 0, 0, 0.85)";   
-    document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, 0.85)";  
-} 
+function startGame() {
+  startMessage.remove();
+  randomFlag();
+  document.querySelector(".hero").style.backgroundColor = "rgba(0, 0, 0, 0.85)";
+  document.querySelector("nav").style.backgroundColor = "rgba(0, 0, 0, 0.85)";
+  startMessage.remove();
+}
 
 async function randomFlag() {
-    try {
-        const response = await fetch("https://geothusiasm-0gow.onrender.com/countries/random");
-        const data = await response.json();
-        
-        document.querySelector("#guess-flag").style.display = "block";
+  try {
+    const response = await fetch(
+      "https://geothusiasm-0gow.onrender.com/countries/random"
+    );
+    const data = await response.json();
 
-        console.log(data.flag)
-        const flag = data.flag;
-        
-        const flagImg = document.querySelector("#flag-img");
-        flagImg.setAttribute("src", flag);
-        
-        const fact = data.fact;
-        const rightAnswer = data.country
+    document.querySelector("#guess-flag").style.display = "block";
 
-        const wrongAnswers = await wrongCountries(rightAnswer);
+    console.log(data.flag);
+    const flag = data.flag;
 
-        //const answers = shuffle([wrongAnswers, rightAnswer]);
+    const flagImg = document.querySelector("#flag-img");
+    flagImg.setAttribute("src", flag);
 
-        const answers = shuffle([
-            wrongAnswers[0],
-            wrongAnswers[1],
-            wrongAnswers[2],
-            rightAnswer,
-          ]);
+    const fact = data.fact;
+    const rightAnswer = data.country;
 
-        //console.log(answers);
+    const wrongAnswers = await wrongCountries(rightAnswer);
 
-        function addToArray(answers) {
-            ans.push(answers);
-        }
-        answers.forEach(addToArray);
+    //const answers = shuffle([wrongAnswers, rightAnswer]);
 
-        //console.log(flag);
-        console.log('Right Answer:', rightAnswer);
-        console.log('Wrong Answers:', wrongAnswers);
+    const answers = shuffle([
+      wrongAnswers[0],
+      wrongAnswers[1],
+      wrongAnswers[2],
+      rightAnswer,
+    ]);
 
-        //console.log(fact);
+    console.log(answers);
 
-    } catch(error) {
-        console.log(error)
+    function addToArray(answers) {
+      ans.push(answers);
     }
+    answers.forEach(addToArray);
+
+    //console.log(flag);
+    console.log("Right Answer:", rightAnswer);
+    console.log("Wrong Answers:", wrongAnswers);
+    createButtons(ans);
+
+    //console.log(fact);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 let ans = [];
 console.log(ans);
 
-
 async function wrongCountries(rightAnswer) {
-    try {
-        const response = await fetch("https://geothusiasm-0gow.onrender.com/countries");
-        const data = await response.json();
-        //console.log(data.country)
+  try {
+    const response = await fetch(
+      "https://geothusiasm-0gow.onrender.com/countries"
+    );
+    const data = await response.json();
+    //console.log(data.country)
 
-        const countries = data.map(countryObj => countryObj.country).filter(country => country !== rightAnswer);
+    const countries = data
+      .map((countryObj) => countryObj.country)
+      .filter((country) => country !== rightAnswer);
 
-        const shuffledCountries = shuffle(countries);
-        return shuffledCountries.slice(0, 3);
-
-    } catch(error) {
-        console.log(error);
-        return [];
-    }
+    const shuffledCountries = shuffle(countries);
+    return shuffledCountries.slice(0, 3);
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
 
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
-
-function startGame () {
-    startMessage.remove();
-
-    createButtons(ans);
-} 
 
 function createButtons() {
-    const container = document.createElement('div')
-    container.classList.add('button-container');
+  const container = document.createElement("div");
+  container.classList.add("button-container");
 
-    const shuffleAnswers = shuffle(ans);
+  const shuffleAnswers = shuffle(ans);
 
-    shuffleAnswers.forEach(answer => {
-        const button = document.createElement('button');
-        button.textContent = answer;
-        button.addEventListener('click', handleButtonClick);
-        container.appendChild(button);
-    });
+  shuffleAnswers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.textContent = answer;
+    button.addEventListener("click", handleButtonClick);
+    container.appendChild(button);
+  });
 
-    const section = document.querySelector('.hero')
-    section.appendChild(container);
+  const section = document.querySelector(".hero");
+  section.appendChild(container);
 }
-
 
 function handleButtonClick(event) {
-    const selectedAnswer = event.target.textContent;
-    console.log('Selected Answer:', selectedAnswer);
-    // Do something with the selected answer
+  const selectedAnswer = event.target.textContent;
+  console.log("Selected Answer:", selectedAnswer);
+  // Do something with the selected answer
 }
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.help');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelector(".help");
 
 const openModal = function () {
- console.log('Button clicked');
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-}
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-}
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-btnsOpenModal.addEventListener('click', openModal);
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnsOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener("keydown", function (e) {
   console.log(e.key);
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
-
-
